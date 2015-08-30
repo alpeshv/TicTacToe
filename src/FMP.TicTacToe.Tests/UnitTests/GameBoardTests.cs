62c4f146a4b1ad5282f4301c5877fe4ac35dfcba
alpesh12212 <alpesh12212@gmail.com>
2015-08-30 11:33:01 +0100

    Exposed gameboard value array as readonly property to simplify tests and updated gameboard tests accordingly

--- src/FMP.TicTacToe.Tests/UnitTests/GameBoardTests.cs
+++ src/FMP.TicTacToe.Tests/UnitTests/GameBoardTests.cs
@@ -28,31 +28,6 @@ namespace FMP.TicTacToe.Tests.UnitTests
             Assert.That(GameBoard.DisplayFormat, Is.EqualTo(DisplayFormat));
         }
 
-        // Next 2 test sets are simlar but with different purposes. We need to rely on toString method to inspect private gameboard value array.
-        // Could have made gameboard value array public to test constructor and toString method independently
-        // Not using AssertGameBoardStatus method in next 2 tests to keep toString method visible in the tests
-        [Test]
-        public void ToString_WhenGameBoardIsBlank_ShouldCreateAFormattedStringWithSpaces()
-        {
-            var gameBoard = new GameBoard(_boardArray);
-
-            var expectedGameBoardString = GenerateGameBoardString(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-            Assert.That(gameBoard.ToString(), Is.EqualTo(expectedGameBoardString));
-        }
-
-        [Test]
-        public void ToString_WhenGameBoardIsNotBlank_ShouldCreateAFormattedStringWithValues()
-        {
-            _boardArray[0, 0] = 'O';
-            _boardArray[0, 2] = 'X';
-            _boardArray[2, 0] = 'X';
-            _boardArray[2, 2] = 'O';
-            var gameBoard = new GameBoard(_boardArray);
-
-            var expectedGameBoardString = GenerateGameBoardString('O', ' ', 'X', ' ', ' ', ' ', 'X', ' ', 'O');
-            Assert.That(gameBoard.ToString(), Is.EqualTo(expectedGameBoardString));
-        }
-
         [Test]
         public void CreatingNewGameBoard_WithEmptyValues_ShouldInitializeGameBoardWithEmptyValues()
         {
@@ -104,6 +79,28 @@ namespace FMP.TicTacToe.Tests.UnitTests
             AssertGameBoardStatus(gameBoard, ' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ');
         }
         
+        [Test]
+        public void ToString_WhenGameBoardIsBlank_ShouldCreateAFormattedStringWithSpaces()
+        {
+            var gameBoard = new GameBoard(_boardArray);
+
+            var expectedGameBoardString = GenerateGameBoardString(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+            Assert.That(gameBoard.ToString(), Is.EqualTo(expectedGameBoardString));
+        }
+
+        [Test]
+        public void ToString_WhenGameBoardIsNotBlank_ShouldCreateAFormattedStringWithValues()
+        {
+            _boardArray[0, 0] = 'O';
+            _boardArray[0, 2] = 'X';
+            _boardArray[2, 0] = 'X';
+            _boardArray[2, 2] = 'O';
+            var gameBoard = new GameBoard(_boardArray);
+
+            var expectedGameBoardString = GenerateGameBoardString('O', ' ', 'X', ' ', ' ', ' ', 'X', ' ', 'O');
+            Assert.That(gameBoard.ToString(), Is.EqualTo(expectedGameBoardString));
+        }
+        
         private static string GenerateGameBoardString(char val11, char val12, char val13, char val21, char val22, char val23, char val31, char val32, char val33)
         {
             return string.Format(DisplayFormat, val11, val12, val13, val21, val22, val23, val31, val32, val33);
@@ -111,8 +108,21 @@ namespace FMP.TicTacToe.Tests.UnitTests
 
         private static void AssertGameBoardStatus(GameBoard gameBoard, char val11, char val12, char val13, char val21, char val22, char val23, char val31, char val32, char val33)
         {
-            var expectedGameBoardString = GenerateGameBoardString(val11, val12, val13, val21, val22, val23, val31, val32, val33);
-            Assert.That(gameBoard.ToString(), Is.EqualTo(expectedGameBoardString));
+            // Could have updated _boardArray and compared it with gameBoardValues rather than comparing 
+            // individual values but that would require same array manipulation as production code
+            // which I wanted to avoid
+            var gameBoardValues = gameBoard.GameBoardValues;
+            Assert.That(gameBoardValues[0,0], Is.EqualTo(val11));
+            Assert.That(gameBoardValues[0,1], Is.EqualTo(val12));
+            Assert.That(gameBoardValues[0,2], Is.EqualTo(val13));
+
+            Assert.That(gameBoardValues[1, 0], Is.EqualTo(val21));
+            Assert.That(gameBoardValues[1, 1], Is.EqualTo(val22));
+            Assert.That(gameBoardValues[1, 2], Is.EqualTo(val23));
+
+            Assert.That(gameBoardValues[2, 0], Is.EqualTo(val31));
+            Assert.That(gameBoardValues[2, 1], Is.EqualTo(val32));
+            Assert.That(gameBoardValues[2, 2], Is.EqualTo(val33));
         }
     }
 }
--- src/FMP.TicTacToe/GameBoard.cs
+++ src/FMP.TicTacToe/GameBoard.cs
@@ -10,36 +10,36 @@ namespace FMP.TicTacToe
     {
         // Display format could be injected by game controller but I think its better to leave it on GameBoard itself
         public const string DisplayFormat = "{0}|{1}|{2}\n_ _ _\n{3}|{4}|{5}\n_ _ _\n{6}|{7}|{8}";
-        private char[,] _gameBoardValues;
+        public char[,] GameBoardValues { get; private set; }
 
         public GameBoard(char[,] initialValues)
         {
-            _gameBoardValues = initialValues;
+            GameBoardValues = initialValues;
         }
 
         public override string ToString()
         {
             return string.Format(DisplayFormat,
-                _gameBoardValues[0, 0],
-                _gameBoardValues[0, 1],
-                _gameBoardValues[0, 2],
-                _gameBoardValues[1, 0],
-                _gameBoardValues[1, 1],
-                _gameBoardValues[1, 2],
-                _gameBoardValues[2, 0],
-                _gameBoardValues[2, 1],
-                _gameBoardValues[2, 2]);
+                GameBoardValues[0, 0],
+                GameBoardValues[0, 1],
+                GameBoardValues[0, 2],
+                GameBoardValues[1, 0],
+                GameBoardValues[1, 1],
+                GameBoardValues[1, 2],
+                GameBoardValues[2, 0],
+                GameBoardValues[2, 1],
+                GameBoardValues[2, 2]);
         }
 
         public void Play(Player player, Tuple<int, int> playLocation)
         {
-            if (_gameBoardValues[playLocation.Item1 - 1, playLocation.Item2 - 1] == ' ')
-                _gameBoardValues[playLocation.Item1 - 1, playLocation.Item2 - 1] = player.PlayCharacter;
+            if (GameBoardValues[playLocation.Item1 - 1, playLocation.Item2 - 1] == ' ')
+                GameBoardValues[playLocation.Item1 - 1, playLocation.Item2 - 1] = player.PlayCharacter;
         }
 
         public void Reset()
         {
-            _gameBoardValues = new[,]
+            GameBoardValues = new[,]
             {
                 { ' ', ' ', ' ' },
                 { ' ', ' ', ' ' },
